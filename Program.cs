20c1c8eb2ba5639606b53fdafb1ac67a5cf295f0
Andrey-Nekrasov-coder <111432108+Andrey-Nekrasov-coder@users.noreply.github.com>
2022-09-03 14:07:26 +0700

    Task2

--- Program.cs
+++ Program.cs
@@ -2,11 +2,11 @@
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
-    if (a>b)
-Console.WriteLine("max="+a);
-    else 
-Console.WriteLine("max="+b);
-    if (b>a)
-Console.WriteLine("min="+a);
-    else 
-Console.WriteLine("min="+b);
+Console.WriteLine("Введите второе число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if ((a > b ) && (b > c))
+Console.WriteLine((a,b,c) + "->"+ a);
+if ((b > a ) && (a > c))
+Console.WriteLine((a,b,c) + "->"+ b);
+if ((c > b ) && (b > a))
+Console.WriteLine((a,b,c) + "->"+ c);
